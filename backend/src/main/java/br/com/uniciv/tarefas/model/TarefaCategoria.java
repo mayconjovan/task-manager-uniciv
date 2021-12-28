cb7f1dfed8eb81b527defb604f9285d766a39e92
Maycon J Pereira <mayconjova@gmail.com>
2021-12-28 11:43:42 -0300

    Project structured

---
+++ backend/src/main/java/br/com/uniciv/tarefas/model/TarefaCategoria.java
@@ -0,0 +1,16 @@
+package br.com.uniciv.tarefas.model;
+
+public class TarefaCategoria {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ backend/src/main/java/br/com/uniciv/tarefas/model/TarefaStatus.java
@@ -0,0 +1,7 @@
+package br.com.uniciv.tarefas.model;
+
+public enum TarefaStatus {
+	
+	ABERTO, EM_ANDAMENTO, CONCLUIDA, CANCELADA;
+
+}
