0d062971a1866f51a11452467b7f9c6faa3028de
Maycon J Pereira <mayconjova@gmail.com>
2021-12-30 14:59:47 -0300

    Added bean validation for DTO request

--- backend/src/main/java/br/com/uniciv/tarefas/controller/request/TarefaRequest.java
+++ backend/src/main/java/br/com/uniciv/tarefas/controller/request/TarefaRequest.java
@@ -2,12 +2,29 @@ package br.com.uniciv.tarefas.controller.request;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class TarefaRequest {
 
 	private Integer id;
+
+	@Size(min = 5, max = 150, message = "tarefa.descricao.size")
+	@NotBlank(message = "{tarefa.descricao.not-blank}")
 	private String descricao;
+	
+	@FutureOrPresent(message = "{tarefa.descricao.future-or-present}")
 	private LocalDate dataEntrega;
+	
+	@NotNull(message = "{tarefa.categoria.not-null}")
+	@Min(value=1, message="{tarefa.categoria.min}")
 	private Integer categoriaId;
+	
+	@NotNull(message = "{tarefa.usuario.not-null}")
+	@Min(value=1, message="{tarefa.usuario.min}")
 	private Integer usuarioID;
 
 	public Integer getId() {
